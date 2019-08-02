5753b66ec898e5d4c71b958dfbd852cd139ffca5
erdenetsogt <a.erdenetsogt@gmail.com>
2019-08-02 12:20:09 +0800

    admin navbar

--- src/admin/App.js
+++ src/admin/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { Route, Switch } from "react-router-dom";
 import MenuCategory from './MenuCategory';
 import Menu from './Menu';
+import Navbar from './Navbar';
 
-function AdminApp() {    
+function AdminApp() {
     return (
-        <Switch>            
-            <Route path="/admin/menuCategory" exact component={MenuCategory} />
-            <Route path="/admin/menu" exact component={Menu} />            
-        </Switch>
+        <>
+            <Navbar />
+            <div style={{height: '6rem'}}/>
+            <Switch>
+                <Route path="/admin/menuCategory" exact component={MenuCategory} />
+                <Route path="/admin/menu" exact component={Menu} />
+            </Switch>
+        </>
     );
 }
 
---
+++ src/admin/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+function Navbar() {
+    return (
+        <nav class="navbar navbar-dark bg-dark fixed-top navbar-expand-lg navbar-light bg-light">            
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <Link class="nav-link" to="/admin/menuCategory">Цэсний ангилал <span class="sr-only">(current)</span></Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link class="nav-link" to="/admin/menu">Цэс</Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link class="nav-link" to="/admin/blog">Блог</Link>
+                    </li>
+                </ul>                
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
